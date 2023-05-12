d3679e31daa1fac7e82981f3fbb1189657f8e2ea
hasan-kayan <hasankayan2000@hotmail.com>
2023-05-12 12:06:44 +0300

    Start of First Project

---
+++ ASP_.NET_Core_First/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASP_.NET_Core_First.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
