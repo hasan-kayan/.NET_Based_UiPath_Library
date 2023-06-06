658f764f679a2f38433d5fcaf689c1a06552856b
hasan-kayan <hasankayan2000@hotmail.com>
2023-06-06 13:51:52 +0300

    Open and create Workbook events

---
+++ ExcelReadActivity/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel; // .NET version excel usage microsoft lib
+// For Excel applications C# has  Microsoft.Office.Interop.Excel namespace, namespace using interface architecture
+// Namespace includes Application class which represents Excel Application
+
+
+namespace ExcelReadActivity
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Application app = new Application();  // Creating new instance of the ' Application Class '. If you have to declare again remember you it will give error 
+            // Go on the error and choose namespace 
+            app.Visible = true; // Set visible property of the "Application" object to "true"
+
+
+            Workbook sampleWorkbook = app.Workbooks.Add(); // Creating Workbook, added application to manage
+
+
+            // So far we have created an empty Workbook, app is representing Excel file so we have created a new Excel file 
+
+            Workbook exsistingWorkbook = app.Workbooks.Open(@"C:\Users\hasan\Desktop\New Microsoft Excel Worksheet.xlsx "); // Thats how you can open an existing workbook 
+
+            // So far we have created an empty workbook and opened an exsiting one 
+
+
+
+
+        }
+    }
+}
