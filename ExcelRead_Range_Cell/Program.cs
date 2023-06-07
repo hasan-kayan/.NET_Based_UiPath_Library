2515ddf45aa85f1938a5a156052863953899b2fc
hasan-kayan <hasankayan2000@hotmail.com>
2023-06-07 18:11:15 +0300

    Range Ra

--- ExcelRead_Range_Cell/Program.cs
+++ ExcelRead_Range_Cell/Program.cs
@@ -1,47 +1,54 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using static System.Net.Mime.MediaTypeNames;
-using Microsoft.Office.Interop.Excel;
-using System.Runtime.InteropServices;
+using Excel = Microsoft.Office.Interop.Excel;
 
-namespace ExcelRead_Range_Cell
+namespace ExcelReadApp
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please Enter The File Path:");
+            Console.WriteLine("Enter the range to read (e.g., A1:B5):");
+            string range = Console.ReadLine();
+
+            Console.WriteLine("Enter the path to the Excel file:");
             string filePath = Console.ReadLine();
 
-            Console.WriteLine("1)- Cell Reading , 2)- Range Reading"); ;
+            Excel.Application excelApp = new Excel.Application();
+            Excel.Workbook workbook = excelApp.Workbooks.Open(filePath);
+            Excel.Worksheet worksheet = workbook.ActiveSheet;
 
-            int caseData = Int32.Parse(Console.ReadLine());
-            ;
-
-            switch (caseData)
+            try
             {
-                case 1:
-                    Console.WriteLine("Please Enter Data and Cell You want to read:");
-                    string cellID = Console.ReadLine();
-                    
+                Excel.Range excelRange = worksheet.Range[range];
+                object[,] values = excelRange.Value;
 
-                    break;
+                int rowCount = values.GetLength(0);
+                int columnCount = values.GetLength(1);
 
-               case 2:
-                    Console.WriteLine("Please enter range you want to read:");
-                    string range = Console.ReadLine();
-                    break;
+                Console.WriteLine($"Reading range: {range}");
+                Console.WriteLine();
 
+                for (int row = 1; row <= rowCount; row++)
+                {
+                    for (int column = 1; column <= columnCount; column++)
+                    {
+                        object value = values[row, column];
+                        Console.Write(value + "\t");
+                    }
+                    Console.WriteLine();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            finally
+            {
+                workbook.Close();
+                excelApp.Quit();
             }
 
-
-
-
-
+            Console.ReadLine();
         }
     }
 }
-
