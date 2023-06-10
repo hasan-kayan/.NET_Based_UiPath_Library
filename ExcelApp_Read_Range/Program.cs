64d937680e25eb0290a144bcdefa87f595048546
hasan-kayan <hasankayan2000@hotmail.com>
2023-06-11 00:06:49 +0300

    Reload

--- ExcelApp_Read_Range/Program.cs
+++ ExcelApp_Read_Range/Program.cs
@@ -4,7 +4,6 @@ using static System.Net.Mime.MediaTypeNames;
 using Microsoft.Office.Interop.Excel;
 using System.Runtime.InteropServices;
 using Application = Microsoft.Office.Interop.Excel.Application;
-using System.Text;
 
 namespace ExcelReadApp
 {
@@ -12,40 +11,41 @@ namespace ExcelReadApp
     {
         static void Main(string[] args)
         {
-            Console.OutputEncoding = Encoding.UTF8;
-            Console.InputEncoding = Encoding.UTF8;
+            // activities can be opened ins VS
 
             Console.WriteLine("Enter the path to the Excel file:");
             string filePath = Console.ReadLine();
 
-            filePath = Encoding.Default.GetString(Encoding.UTF8.GetBytes(filePath));
-
-            Console.WriteLine("Enter the range to read (e.g., A1:B5):");
+            Console.WriteLine("Enter the range to read (e.g., A1:B5):"); // Range girilmezse 
             string range = Console.ReadLine();
 
             Console.WriteLine("Please enter the worksheet name you want to work on:");
             string worksheetName = Console.ReadLine();
+            
 
             Application app = new Application();
             app.Visible = false;
 
-            Workbook existingWorkbook = app.Workbooks.Open(filePath);
-            Worksheet worksheet = existingWorkbook.Worksheets[worksheetName];
+            Workbook existingWorkbook = app.Workbooks.Open(filePath); // Open file to read
+            Worksheet worksheet = existingWorkbook.Worksheets[worksheetName]; // Declare Worksheet
+            
 
-            try
+
+            try // For possibel mistakes, try-catch 
             {
-                Range excelRange = worksheet.Range[range];
-                object[,] values = excelRange.Value;
+                Range excelRange = worksheet.Range[range]; // Range structre is embedded into lib 
+                object[,] values = excelRange.Value; // Object arry to keep readed values 
 
+                // etrieves the number of rows and columns 
                 int rowCount = values.GetLength(0);
                 int columnCount = values.GetLength(1);
 
                 Console.WriteLine($"Reading range: {range}");
                 Console.WriteLine();
 
-                for (int row = 1; row <= rowCount; row++)
+                for (int row = 1; row <= rowCount; row++) //  row count and loop
                 {
-                    for (int column = 1; column <= columnCount; column++)
+                    for (int column = 1; column <= columnCount; column++) // column count loop 
                     {
                         object value = values[row, column];
                         Console.Write(value + "\t");
@@ -53,7 +53,7 @@ namespace ExcelReadApp
                     Console.WriteLine();
                 }
             }
-            catch (Exception ex)
+            catch (Exception ex) // Possible error catch 
             {
                 Console.WriteLine("Error: " + ex.Message);
             }
@@ -67,3 +67,7 @@ namespace ExcelReadApp
         }
     }
 }
+
+// C:\Users\hasan\Desktop\excel applications try\deneme.xlsx
+// BIG DATA //
+// C:\Users\hasan\Desktop\excel applications try\Halkbank\TC Hazine ve Maliye Bakanlığı yazısı - İhracat bedelleri+IBKB_V2_Exa (YENİ)_995_03.30.2023_11.50.47.xlsx
