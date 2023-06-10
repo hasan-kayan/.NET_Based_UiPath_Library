a0a00ff6ed0e8c0c896f4bb179b4b0ce9d150ffe
hasan-kayan <hasankayan2000@hotmail.com>
2023-06-10 23:48:15 +0300

    Working State

--- ExcelApp_Read_Range/Program.cs
+++ ExcelApp_Read_Range/Program.cs
@@ -12,45 +12,40 @@ namespace ExcelReadApp
     {
         static void Main(string[] args)
         {
-
             Console.OutputEncoding = Encoding.UTF8;
-            Console.InputEncoding = Encoding.UTF8; 
-
-            // activities can be opened ins VS
+            Console.InputEncoding = Encoding.UTF8;
 
             Console.WriteLine("Enter the path to the Excel file:");
             string filePath = Console.ReadLine();
 
-            Console.WriteLine("Enter the range to read (e.g., A1:B5):"); // Range girilmezse 
+            filePath = Encoding.Default.GetString(Encoding.UTF8.GetBytes(filePath));
+
+            Console.WriteLine("Enter the range to read (e.g., A1:B5):");
             string range = Console.ReadLine();
 
             Console.WriteLine("Please enter the worksheet name you want to work on:");
             string worksheetName = Console.ReadLine();
-            
 
             Application app = new Application();
             app.Visible = false;
 
-            Workbook existingWorkbook = app.Workbooks.Open(filePath); // Open file to read
-            Worksheet worksheet = existingWorkbook.Worksheets[worksheetName]; // Declare Worksheet
-            
+            Workbook existingWorkbook = app.Workbooks.Open(filePath);
+            Worksheet worksheet = existingWorkbook.Worksheets[worksheetName];
 
-
-            try // For possibel mistakes, try-catch 
+            try
             {
-                Range excelRange = worksheet.Range[range]; // Range structre is embedded into lib 
-                object[,] values = excelRange.Value; // Object arry to keep readed values 
+                Range excelRange = worksheet.Range[range];
+                object[,] values = excelRange.Value;
 
-                // etrieves the number of rows and columns 
                 int rowCount = values.GetLength(0);
                 int columnCount = values.GetLength(1);
 
                 Console.WriteLine($"Reading range: {range}");
                 Console.WriteLine();
 
-                for (int row = 1; row <= rowCount; row++) //  row count and loop
+                for (int row = 1; row <= rowCount; row++)
                 {
-                    for (int column = 1; column <= columnCount; column++) // column count loop 
+                    for (int column = 1; column <= columnCount; column++)
                     {
                         object value = values[row, column];
                         Console.Write(value + "\t");
@@ -58,7 +53,7 @@ namespace ExcelReadApp
                     Console.WriteLine();
                 }
             }
-            catch (Exception ex) // Possible error catch 
+            catch (Exception ex)
             {
                 Console.WriteLine("Error: " + ex.Message);
             }
@@ -72,7 +67,3 @@ namespace ExcelReadApp
         }
     }
 }
-
-// C:\Users\hasan\Desktop\excel applications try\deneme.xlsx
-// BIG DATA //
-// C:\Users\hasan\Desktop\excel applications try\Halkbank\TC Hazine ve Maliye Bakanlığı yazısı - İhracat bedelleri+IBKB_V2_Exa (YENİ)_995_03.30.2023_11.50.47.xlsx
