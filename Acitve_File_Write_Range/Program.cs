88d10bd7d6eec994e6e37d43cf6f3448813aed9a
hasan-kayan <hasankayan2000@hotmail.com>
2023-06-13 12:53:34 +0300

    Formated

--- Acitve_File_Write_Range/Program.cs
+++ Acitve_File_Write_Range/Program.cs
@@ -8,14 +8,6 @@ namespace ExcelDataReader
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the column name you want to add the data:");
-            string columnName = Console.ReadLine();
-
-            Console.WriteLine("Enter data you want to add, separated by '|':");
-            string input = Console.ReadLine();
-
-            string[] data = input.Split('|');
-            Console.WriteLine(data);
 
             // Create an Excel application object
             Application excelApp = null;
@@ -44,22 +36,37 @@ namespace ExcelDataReader
                 return;
             }
 
-            Console.WriteLine("Workbook detected.");
-
-            Console.WriteLine(workbook.Name);
-
-
+            Console.WriteLine("Workbook detected ' " + workbook.Name + " '" );
 
             // Get the active worksheet
             Worksheet worksheet = workbook.ActiveSheet;
+            Console.WriteLine("You are about to write data into : " + worksheet.Name);
+
+
+
+            Console.WriteLine("Enter the column name you want to add the data:");
+            string columnName = Console.ReadLine();
+
+            Console.WriteLine("Enter data you want to add, separated by '|':");
+            string input = Console.ReadLine();
+
+            string[] data = input.Split('|');
+            Console.WriteLine(data);
+
+            // Enter Data Into Column
+          
 
             for (int i = 0; i < data.Length; i++)
             {
                 worksheet.Range[columnName + (2 + i)].Value = data[i];
+                Console.WriteLine("Data Writen Into : " + columnName + (2 + i) + " / Writen Data is: " + data[i]);
+
             }
 
-            workbook.Save();
 
+            Console.WriteLine("All data inserted succesfully !");
+            workbook.Save();
+            Console.WriteLine("Workbook" + workbook.Name + " Saved Succesfully");
             Console.WriteLine("Press any key to exit.");
             Console.ReadKey();
         }
