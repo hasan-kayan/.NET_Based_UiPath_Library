6e496199f7a26b26df77bcc919c8c03260bb5d8a
hasan-kayan <hasankayan2000@hotmail.com>
2023-06-13 10:28:54 +0300

    Have no error state

--- Acitve_File_Write_Range/Program.cs
+++ Acitve_File_Write_Range/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Runtime.InteropServices;
 using Microsoft.Office.Interop.Excel;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace ExcelDataWriter
 {
@@ -16,56 +15,45 @@ namespace ExcelDataWriter
             Console.WriteLine("Enter the name of the worksheet:");
             string worksheetName = Console.ReadLine();
 
-            // Prompt the user to enter the range where the data will be written
-            Console.WriteLine("Enter the range (e.g., A1:B3) where the data will be written:");
-            string range = Console.ReadLine();
+            // Prompt the user to enter the column name where the data will be written
+            Console.WriteLine("Enter the column name (e.g., A) where the data will be written:");
+            string columnName = Console.ReadLine();
 
             // Prompt the user to enter the data to be written
-            Console.WriteLine("Enter the data to be written (comma-separated values for each row):");
-            string dataInput = Console.ReadLine();
+            Console.WriteLine("Enter the data to be written, separete data by comma '|':");
+            String data = Console.ReadLine();
 
-            // Split the data into rows
-            string[] dataRows = dataInput.Split(',');
+            string[] Data = data.Split('|'); // One line string will be seperated | bunula ayır 
 
-            // Get the currently active Excel application
-            Application excelApp = (Application)Marshal.GetActiveObject("Excel.Application");
+
+
+            // Create an Excel application object
+            Microsoft.Office.Interop.Excel.Application excelApp = null;
+            try
+            {
+                excelApp = (Microsoft.Office.Interop.Excel.Application)Marshal.GetActiveObject("Excel.Application");
+            }
+            catch (COMException)
+            {
+                Console.WriteLine("No active Excel instance found.");
+                return;
+            }
+
+            Console.WriteLine("Excel Detected");
+
 
             // Get the active workbook
             Workbook workbook = excelApp.ActiveWorkbook;
-
-            // Get the worksheet by name
-            Worksheet worksheet = (Worksheet)workbook.Sheets[worksheetName];
-
-            // Get the range to write the data
-            Range writeRange = worksheet.Range[range];
-
-            // Convert the data into a 2D array
-            int rowCount = dataRows.Length;
-            int columnCount = range.Split(':').Length / 2;
-            object[,] data = new object[rowCount, columnCount];
-
-            for (int row = 0; row < rowCount; row++)
+            if (workbook == null)
             {
-                string[] rowData = dataRows[row].Split(',');
-                for (int col = 0; col < columnCount; col++)
-                {
-                    data[row, col] = rowData[col];
-                }
+                Console.WriteLine("No open workbook found.");
+                return;
             }
+            Console.WriteLine("Workbook Detected");
 
-            // Write the data to the range
-            writeRange.Value = data;
+            // Get the active worksheet
+            Worksheet worksheet = workbook.ActiveSheet;
 
-            // Clean up Excel objects
-            Marshal.ReleaseComObject(writeRange);
-            Marshal.ReleaseComObject(worksheet);
-            Marshal.ReleaseComObject(workbook);
-            Marshal.ReleaseComObject(excelApp);
-
-            Console.WriteLine("Data written successfully.");
-
-            Console.WriteLine("Press any key to exit.");
-            Console.ReadKey();
         }
     }
 }
