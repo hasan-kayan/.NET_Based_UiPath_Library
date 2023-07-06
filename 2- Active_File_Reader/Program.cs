bfd09f1652f0f47843fad884dbc64fa831bb9b29
hasan-kayan <hasankayan2000@hotmail.com>
2023-07-07 01:13:03 +0300

    Half data transection completed

--- 2- Active_File_Reader/Program.cs
+++ 2- Active_File_Reader/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using Microsoft.Office.Interop.Excel;
-using System.Data;
-using DataTable = System.Data.DataTable;
+using System.IO;
 
 class Program
 {
@@ -9,13 +8,25 @@ class Program
     {
         string excelFilePath = @"C:\Users\hasan\Desktop\Büyük Excel\Halkbank\TC Hazine ve Maliye Bakanlığı yazısı - İhracat bedelleri+IBKB_V2_Exa (YENİ)_995_03.30.2023_11.50.47.xlsx";
         string sheetName = "TC Hazine ve Maliye Bakanlığı y";
-        string range = "B4:BJ200000"; // Örnek aralık
-        int rowsPerIteration = 4000;
+        int rowsPerIteration = 40000;
+
+        string startColumnName = "A";
+        int startColumnValue = 1;
+
+        string endColumnName = "BJ";
+        int endColumnValue = 20000;
+
+        int endColumnValueFirst = startColumnValue + 4000;
+
+        float rowCount = endColumnValue - startColumnValue;
+        int loopCount = (int)rowCount;
 
         try
         {
             Console.WriteLine("Excel dosyası açılıyor...");
             Application excelApp = new Application();
+            excelApp.Visible = true;
+
             Workbook workbook = null;
             Worksheet worksheet = null;
 
@@ -37,90 +48,45 @@ class Program
                 worksheet = workbook.Sheets[sheetName];
             }
 
-            if (worksheet != null)
+            // Yeni bir Excel dosyası oluştur
+            string newExcelFilePath = Path.Combine(Path.GetDirectoryName(excelFilePath), "copy.xlsx");
+            Workbook newWorkbook = excelApp.Workbooks.Add();
+            Worksheet newWorksheet = newWorkbook.Sheets[1];
+
+            for (int i = 1; i <= loopCount; i++)
             {
-                Console.WriteLine("Sayfa bulundu. Hücre değerleri alınıyor...");
-                Range cells = worksheet.Range[range];
-                int rowCount = cells.Rows.Count;
-                int currentRow = 1;
+                string startRange = startColumnName + startColumnValue + ":" + endColumnName + endColumnValueFirst;
 
-                while (currentRow <= rowCount)
-                {
-                    int endRow = Math.Min(currentRow + rowsPerIteration - 1, rowCount);
-                    string currentRange = $"B{currentRow}:BJ{endRow}";
-                    Range currentCells = worksheet.Range[currentRange];
-                    object[,] cellValues = (object[,])currentCells.Value;
+                Console.WriteLine(startRange + " okunuyor...");
 
-                    if (cellValues != null)
-                    {
-                        int columnCount = cellValues.GetLength(1);
+                // Verileri oku
+                Range range = worksheet.Range[startRange];
+                object[,] values = (object[,])range.Value;
 
-                        DataTable dataTable = new DataTable();
+                // Verileri copy.xlsx dosyasına yaz
+                Range newRange = newWorksheet.Range[startColumnName + (startColumnValue + 1) + ":" + endColumnName + (endColumnValueFirst + 1)];
+                newRange.Value = values;
 
-                        // DataTable'a sütunları ekle
-                        for (int col = 1; col <= columnCount; col++)
-                        {
-                            dataTable.Columns.Add($"Column{col}");
-                        }
+                // Veri tutma değerlerini sıfırla
+                values = null;
+                range = null;
 
-                        // Hücre değerlerini DataTable'a ekle
-                        for (int row = 1; row <= rowsPerIteration; row++)
-                        {
-                            if (currentRow > rowCount)
-                                break;
-
-                            DataRow dataRow = dataTable.NewRow();
-
-                            for (int col = 1; col <= columnCount; col++)
-                            {
-                                object cellValue = cellValues[row, col];
-                                dataRow[col - 1] = cellValue;
-                            }
-
-                            dataTable.Rows.Add(dataRow);
-                            currentRow++;
-                        }
-
-                        Console.WriteLine("Hücre değerleri alındı. DataTable çıktısı:");
-                        PrintDataTable(dataTable);
-                    }
-                    else
-                    {
-                        Console.WriteLine("Belirtilen aralıkta hücre değeri bulunamadı.");
-                    }
-                }
-            }
-            else
-            {
-                Console.WriteLine("Belirtilen sayfa adı bulunamadı.");
+                startColumnValue = endColumnValueFirst + 1;
+                endColumnValueFirst += rowsPerIteration;
             }
 
+            // Excel dosyalarını kapat ve kaynakları serbest bırak
             workbook.Close();
+            newWorkbook.Save();
+            newWorkbook.Close();
             excelApp.Quit();
+
+            Console.WriteLine("İşlem tamamlandı. Yeni Excel dosyası: " + newExcelFilePath);
         }
         catch (Exception ex)
         {
-            Console.WriteLine("Bir hata oluştu: " + ex.Message);
+            Console.WriteLine("Hata: " + ex.Message);
         }
-
-        Console.ReadLine();
-    }
-
-    static void PrintDataTable(DataTable dataTable) 
-    {
-        foreach (DataColumn column in dataTable.Columns)
-        {
-            Console.Write($"{column.ColumnName}\t");
-        }
-        Console.WriteLine();
-
-        foreach (DataRow row in dataTable.Rows)
-        {
-            foreach (var item in row.ItemArray)
-            {
-                Console.Write($"{item}\t");
-            }
-            Console.WriteLine();
-        }
+        Console.ReadKey();
     }
 }
