68c4756c4d16d45f40a350f06b576fcdcac128b7
hasan-kayan <hasankayan2000@hotmail.com>
2023-07-10 12:07:06 +0300

    All in one console

---
+++ ALL IN ONE/All big data reading/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace All_big_data_reading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Excel uygulamasını başlat
+            Excel.Application excelApp = new Excel.Application();
+            excelApp.Visible = true;
+
+            // Aktif çalışma kitabını ve çalışma sayfasını al
+            Excel.Workbook workbook = excelApp.Workbooks.Open(excelFilePath);
+            Excel.Worksheet worksheet = workbook.Sheets[sheetName];
+        }
+    }
+}
