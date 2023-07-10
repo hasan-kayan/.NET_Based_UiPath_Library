ea49dcdf162c870daa439ee47f42c99b4a84c953
hasan-kayan <hasankayan2000@hotmail.com>
2023-07-10 14:57:08 +0300

    Conosle Data Log

--- ALL IN ONE/All big data reading/Program.cs
+++ ALL IN ONE/All big data reading/Program.cs
@@ -18,13 +18,9 @@ namespace All_big_data_reading
     {
         static void Main(string[] args)
         {
-            string excelFilePath = @"";
-
-            string sheetName = "";
-
-            string range = "";
-
-
+            string excelFilePath = @"C:\Users\hasan\Desktop\Büyük Excel\Halkbank\TC Hazine ve Maliye Bakanlığı yazısı - İhracat bedelleri+IBKB_V2_Exa_Robotik Süreç (1).xlsx";
+            string sheetName = "TC Hazine ve Maliye Bakanlığı y";
+            string range = "B4:BJ24000";
 
             // Excel uygulamasını başlat
             Console.WriteLine("New Application Starting...");
@@ -35,7 +31,6 @@ namespace All_big_data_reading
             Console.WriteLine("Workbook detection...");
             Excel.Workbook workbook = excelApp.Workbooks.Open(excelFilePath);
             Excel.Worksheet worksheet = workbook.Sheets[sheetName];
-
 
             Excel.Worksheet newWorksheet = null;
 
@@ -62,85 +57,91 @@ namespace All_big_data_reading
                     SkipBlanks: false, Transpose: false);
 
                 Console.WriteLine("Page Copied Successfully!");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Error occurred: {ex.Message}");
-            }
 
+                // Yeni sayfayı aktif hale getirin
+                newWorksheet.Activate();
 
-            // Read the data
-            Excel.Range excelRange = null;
+                // Read the data
+                Excel.Range excelRange = null;
 
-            try
-            {
-                // Read data from the new worksheet
-                if (string.IsNullOrWhiteSpace(range))
+                // Yeni çalışma sayfasının null olup olmadığını kontrol edin
+                if (newWorksheet != null)
                 {
-                    excelRange = newWorksheet.UsedRange;
+                    // Read data from the new worksheet
+                    if (string.IsNullOrWhiteSpace(range))
+                    {
+                        Console.WriteLine("All data reading");
+                        excelRange = newWorksheet.UsedRange;
+                    }
+                    else
+                    {
+                        Console.WriteLine("The range : " + range + "is reading");
+                        excelRange = newWorksheet.Range[range];
+                    }
+
+                    // Continue with reading the data and creating DataTable
+                    Console.WriteLine("Data Table Creating, data reading...");
+                    object[,] excelData = (object[,])excelRange.Value;
+
+                    // Create a DataTable and transfer the data
+                    DataTable dataTable = new DataTable();
+                    int rowCount = excelData.GetLength(0);
+                    int columnCount = excelData.GetLength(1);
+
+                    Console.WriteLine("Data Table created Succesfully");
+
+                    for (int col = 1; col <= columnCount; col++)
+                    {
+                        string columnName = excelData[1, col]?.ToString() ?? $"Column{col}";
+                        dataTable.Columns.Add(columnName);
+                    }
+
+                    for (int row = 2; row <= rowCount; row++)
+                    {
+                        DataRow dataRow = dataTable.NewRow();
+                        for (int col = 1; col <= columnCount; col++)
+                        {
+                            dataRow[col - 1] = excelData[row, col];
+                        }
+                        dataTable.Rows.Add(dataRow);
+                    }
+
+                    // Use the DataTable as needed
+                    Console.WriteLine("Data Table Content:");
+                    foreach (DataRow row in dataTable.Rows)
+                    {
+                        foreach (DataColumn col in dataTable.Columns)
+                        {
+                            Console.Write(row[col] + "\t");
+                        }
+                        Console.WriteLine();
+                    }
                 }
                 else
                 {
-                    excelRange = newWorksheet.Range[range];
+                    Console.WriteLine("New worksheet is null. Cannot proceed with data reading.");
                 }
-
-                // Continue with reading the data and creating DataTable
-                object[,] excelData = (object[,])excelRange.Value;
-
-                // Create a DataTable and transfer the data
-                DataTable dataTable = new DataTable();
-                int rowCount = excelData.GetLength(0);
-                int columnCount = excelData.GetLength(1);
-
-                for (int col = 1; col <= columnCount; col++)
-                {
-                    string columnName = excelData[1, col]?.ToString() ?? $"Column{col}";
-                    dataTable.Columns.Add(columnName);
-                }
-
-                for (int row = 2; row <= rowCount; row++)
-                {
-                    DataRow dataRow = dataTable.NewRow();
-                    for (int col = 1; col <= columnCount; col++)
-                    {
-                        dataRow[col - 1] = excelData[row, col];
-                    }
-                    dataTable.Rows.Add(dataRow);
-                }
-
-                // Use the DataTable as needed
             }
             catch (Exception ex)
             {
                 Console.WriteLine($"Error occurred: {ex.Message}");
             }
 
+            // Serbest bırakma işlemleri
+            
+         
+            workbook?.Close();
+            excelApp?.Quit();
+
+           
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(newWorksheet);
             System.Runtime.InteropServices.Marshal.ReleaseComObject(worksheet);
             System.Runtime.InteropServices.Marshal.ReleaseComObject(workbook);
             System.Runtime.InteropServices.Marshal.ReleaseComObject(excelApp);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            Console.ReadKey();
         }
     }
-    }
-
+}
