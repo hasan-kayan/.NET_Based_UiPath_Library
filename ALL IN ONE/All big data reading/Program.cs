487def448e2210025ba34b520c169da3c3fbda33
hasan-kayan <hasankayan2000@hotmail.com>
2023-07-10 15:49:11 +0300

    120K At Once GREAT SUCCESS

--- ALL IN ONE/All big data reading/Program.cs
+++ ALL IN ONE/All big data reading/Program.cs
@@ -1,16 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Office.Interop.Excel;
-using System.IO;
-
-using DataTable = System.Data.DataTable;
-
-using Excel = Microsoft.Office.Interop.Excel;
-using System.Linq.Expressions;
 using System.Data;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace All_big_data_reading
 {
@@ -21,53 +11,40 @@ namespace All_big_data_reading
             string excelFilePath = @"C:\Users\hasan\Desktop\Büyük Excel\Halkbank\TC Hazine ve Maliye Bakanlığı yazısı - İhracat bedelleri+IBKB_V2_Exa_Robotik Süreç (1).xlsx";
             string sheetName = "TC Hazine ve Maliye Bakanlığı y";
             string range = "B4:BJ120000";
-           
-            // Excel uygulamasını başlat
+
             Console.WriteLine("New Application Starting...");
+
             Excel.Application excelApp = new Excel.Application();
             excelApp.Visible = false;
 
-            // Aktif çalışma kitabını ve çalışma sayfasını al
             Console.WriteLine("Workbook detection...");
             Excel.Workbook workbook = excelApp.Workbooks.Open(excelFilePath);
-            Excel.Worksheet worksheet = workbook.Sheets[sheetName];
-
+            Excel.Worksheet worksheet = null;
             Excel.Worksheet newWorksheet = null;
 
             try
             {
+                worksheet = workbook.Sheets[sheetName];
+
                 Console.WriteLine("Copying Cells");
-                // Tüm hücreleri seç ve kopyala
                 Excel.Range cells = worksheet.Cells;
-                cells.Select();
                 cells.Copy();
+                Console.WriteLine("Cells Copied");
 
                 Console.WriteLine("Creating New Sheet");
-
-                // Yeni çalışma sayfasını ekleyin
                 newWorksheet = workbook.Sheets.Add(After: workbook.ActiveSheet);
-
-                // Yeni sayfanın adını değiştirin
                 newWorksheet.Name = "Copied";
                 Console.WriteLine("New Sheet named 'Copied' Created");
 
-                // Yapıştırma işlemini gerçekleştir
                 Excel.Range pasteRange = newWorksheet.Cells;
                 pasteRange.PasteSpecial(Excel.XlPasteType.xlPasteValues, Excel.XlPasteSpecialOperation.xlPasteSpecialOperationNone,
                     SkipBlanks: false, Transpose: false);
+                Console.WriteLine("Paste Special Completed");
 
-                Console.WriteLine("Page Copied Successfully!");
-
-                // Yeni sayfayı aktif hale getirin
-                newWorksheet.Activate();
-
-             
-                Excel.Range excelRange = null;
-
-                // Yeni çalışma sayfasının null olup olmadığını kontrol edin
                 if (newWorksheet != null)
                 {
-                    // Read data from the new worksheet
+                    Console.WriteLine("Data Table Creating, data reading...");
+                    Excel.Range excelRange;
                     if (string.IsNullOrWhiteSpace(range))
                     {
                         Console.WriteLine("All data reading");
@@ -79,34 +56,49 @@ namespace All_big_data_reading
                         excelRange = newWorksheet.Range[range];
                     }
 
-                    // Continue with reading the data and creating DataTable
-                    Console.WriteLine("Data Table Creating, data reading...");
-                    object[,] excelData = (object[,])excelRange.Value;
+                    int rowCount = excelRange.Rows.Count;
+                    int columnCount = excelRange.Columns.Count;
 
-                    // Create a DataTable and transfer the data
                     DataTable dataTable = new DataTable();
-                    int rowCount = excelData.GetLength(0);
-                    int columnCount = excelData.GetLength(1);
 
-                    Console.WriteLine("Data Table created Succesfully");
-
+                    // Read column names
                     for (int col = 1; col <= columnCount; col++)
                     {
-                        string columnName = excelData[1, col]?.ToString() ?? $"Column{col}";
+                        string columnName = excelRange.Cells[1, col]?.Value?.ToString() ?? $"Column{col}";
                         dataTable.Columns.Add(columnName);
                     }
+                    Console.WriteLine("Column names read");
 
-                    for (int row = 2; row <= rowCount; row++)
+                    // Read data in smaller chunks (rows)
+                    const int batchSize = 1000;
+                    int remainingRows = rowCount - 1;
+                    int startRow = 2;
+
+                    while (remainingRows > 0)
                     {
-                        DataRow dataRow = dataTable.NewRow();
-                        for (int col = 1; col <= columnCount; col++)
+                        int rowsToRead = Math.Min(batchSize, remainingRows);
+                        int endRow = startRow + rowsToRead - 1;
+
+                        Excel.Range dataRange = excelRange.Range[excelRange.Cells[startRow, 1], excelRange.Cells[endRow, columnCount]];
+                        object[,] excelData = (object[,])dataRange.Value;
+                        Console.WriteLine($"Read rows {startRow} to {endRow}");
+
+                        // Transfer data to the DataTable
+                        for (int row = 1; row <= rowsToRead; row++)
                         {
-                            dataRow[col - 1] = excelData[row, col];
+                            DataRow dataRow = dataTable.NewRow();
+                            for (int col = 1; col <= columnCount; col++)
+                            {
+                                dataRow[col - 1] = excelData[row, col];
+                            }
+                            dataTable.Rows.Add(dataRow);
                         }
-                        dataTable.Rows.Add(dataRow);
+                        Console.WriteLine($"Transferred rows {startRow} to {endRow}");
+
+                        startRow += batchSize;
+                        remainingRows -= rowsToRead;
                     }
 
-                    // Use the DataTable as needed
                     Console.WriteLine("Data Table Content:");
                     foreach (DataRow row in dataTable.Rows)
                     {
@@ -126,21 +118,21 @@ namespace All_big_data_reading
             {
                 Console.WriteLine($"Error occurred: {ex.Message}");
             }
+            finally
+            {
+                newWorksheet?.Activate();
+                workbook.Close();
+                excelApp.Quit();
 
-            // Serbest bırakma işlemleri
-            
-         
-            workbook?.Close();
-            excelApp?.Quit();
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(newWorksheet);
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(worksheet);
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(workbook);
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(excelApp);
 
-           
-            System.Runtime.InteropServices.Marshal.ReleaseComObject(newWorksheet);
-            System.Runtime.InteropServices.Marshal.ReleaseComObject(worksheet);
-            System.Runtime.InteropServices.Marshal.ReleaseComObject(workbook);
-            System.Runtime.InteropServices.Marshal.ReleaseComObject(excelApp);
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+            }
 
-            GC.Collect();
-            GC.WaitForPendingFinalizers();
             Console.ReadKey();
         }
     }
